6606e66f01c126175114cd0545fcc2c4f601ee72
Olayemi Obafuwa <obafuwaolayemi@gmail.com>
2023-09-30 12:17:44 +0000

    Port changed

--- todo-list-api/app.js
+++ todo-list-api/app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 
 app.use(bodyParser.json());
-
-// Require and use the task routes
- const tasksRouter = require('./routes/tasks');
-app.use('/api/tasks', tasksRouter);
 
 // Database connection
  const db = require('./db');
 
 app.listen(port, () => {
-	console.log('Server is running on port ${3000}');
+	console.log('Server is running on port ${8080}');
+});
+
+// Require and use the task routes
+const tasksRouter = require('./routes/tasks');
+app.use('/api/tasks', tasksRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
--- todo-list-api/routes/tasks.js
+++ todo-list-api/routes/tasks.js
@@ -1,13 +1,49 @@
 const express = require('express');
 const router = express.Router();
 
-// Define your task-related routes here
+// Temporary array to store tasks (replace with a database later)
+const tasks = [];
 
-module.exports = router;
+// Create a task
+router.post('/', (req, res) => {
+  const { title, description } = req.body;
+  const newTask = { title, description };
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+});
 
-// GET route to test
+// Read all tasks
 router.get('/', (req, res) => {
-	res.json({ message: 'Hello, World!' });
+  res.json(tasks);
+});
+
+// Read a single task by ID
+router.get('/:id', (req, res) => {
+  const taskId = req.params.id;
+  const task = tasks[taskId];
+  if (!task) {
+    return res.status(404).json({ message: 'Task not found' });
+  }
+  res.json(task);
+});
+
+// Update a task by ID
+router.put('/:id', (req, res) => {
+  const taskId = req.params.id;
+  const { title, description } = req.body;
+  const updatedTask = { title, description };
+  tasks[taskId] = updatedTask;
+  res.json(updatedTask);
+});
+
+// Delete a task by ID
+router.delete('/:id', (req, res) => {
+  const taskId = req.params.id;
+  if (!tasks[taskId]) {
+    return res.status(404).json({ message: 'Task not found' });
+  }
+  tasks.splice(taskId, 1);
+  res.status(204).send();
 });
 
 module.exports = router;
